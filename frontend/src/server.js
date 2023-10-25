417c66fd7c0a1f7f3a31a645dd8e9c6606e34d6e
savin8305 <kanhaji0402@gmail.com>
2023-10-25 06:07:59 +0530

    new

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,2 @@
-export const server = "https://eshop-tutorial.vercel.app/api/v2";
-
-
+export const server = "https://shop-dot-v3.vercel.app/api/v2";
+export const backend_url = "https://shop-dot-v3.vercel.app/";
