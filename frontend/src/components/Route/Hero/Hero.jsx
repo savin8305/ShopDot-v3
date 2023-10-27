a58e83b3c76acd68d8f8a09ea29c8e68b8e5bcc6
savin8305 <kanhaji0402@gmail.com>
2023-10-27 21:42:56 +0530

    updated

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -4,15 +4,11 @@ import styles from "../../../styles/styles";
 
 const Hero = () => {
   return (
-    <div
-      className={`relative min-h-[70vh] 800px:min-h-[80vh] bg-black bg-center bg-no-repeat ${styles.noramlFlex}`}
-    >
-      <div
-        className={`${styles.section} w-[90%] 800px:w-[60%] flex justify-between items-center`}
-      >
-        <div className="w-[70%] p-6 text-white">
-          <h1 className="text-4xl 800px:text-6xl font-semibold capitalize mb-4">
-            Best Collection for <br /> Home Decoration
+    <div className={`relative min-h-[70vh] 800px:min-h-[80vh] bg-black bg-center bg-no-repeat ${styles.noramlFlex}`}>
+      <div className={`${styles.section} w-[90%] 800px:w-[60%] flex justify-between items-center`}>
+        <div className="w-full md:w-[70%] p-6 text-white">
+          <h1 className="text-3xl md:text-3xl font-semibold capitalize mb-4 text-justify">
+            Best Collection for  Home Decoration ! made by <q>Akash Vishwakarma !</q>
           </h1>
 
           <Link to="/products" className="inline-block mt-6">
@@ -22,24 +18,23 @@ const Hero = () => {
           </Link>
         </div>
 
-        <div className="w-[30%]"> </div>
+        <div className="hidden md:block md:w-[30%]"></div>
       </div>
-      <div className="w-[50%] mr-10">
-      <video
-            preload="none"
-            playsinline=""
-            loop="loop"
-            muted="muted"
-            className="font-space-grotesk text-base font-normal text-white outline-none box-border relative  flex-grow-0 flex-shrink-0 border-2 rounded-[8px] rounded-tl-[50px] rounded-br-[50px]  mr-4"
-            autoplay=""
-          >
-            
-            <source
-              src="https://res.cloudinary.com/drk8b0kev/video/upload/v1698248359/yuoibqklgn5aoqs57orj.mp4"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
+      <div className="w-full md:w-[50%] md:mr-10">
+        <video
+          preload="none"
+          playsinline=""
+          loop="loop"
+          muted="muted"
+          className="font-space-grotesk text-base font-normal text-white outline-none box-border relative flex-grow-0 flex-shrink-0 border-2 rounded-[8px] rounded-tl-[50px] rounded-br-[50px] mr-4"
+          autoplay=""
+        >
+          <source
+            src="https://res.cloudinary.com/drk8b0kev/video/upload/v1698248359/yuoibqklgn5aoqs57orj.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   );
