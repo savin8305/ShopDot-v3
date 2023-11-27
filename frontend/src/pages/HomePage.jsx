fc6bd464efb4fdbc05379e00c146eea501e81ce1
savin8305 <kanhaji0402@gmail.com>
2023-11-27 23:14:22 +0530

    foter updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,6 @@ import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 import Events from "../components/Events/Events";
-import Sponsored from "../components/Route/Sponsored";
 import Footer from "../components/Layout/Footer";
 
 const HomePage = () => {
@@ -17,7 +16,6 @@ const HomePage = () => {
         <BestDeals />
         <Events />
         <FeaturedProduct />
-        <Sponsored />
         <Footer />
     </div>
   )
