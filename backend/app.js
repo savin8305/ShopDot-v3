babbe0a7cf51cf932ca89edd1f2e94fcceed9a54
savin8305 <kanhaji0402@gmail.com>
2023-11-28 11:46:30 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,8 @@ app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb'}));
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
