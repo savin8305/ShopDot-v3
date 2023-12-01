fd419f67b615586b12bab655d6dd2fefa29eea51
savin8305 <kanhaji0402@gmail.com>
2023-12-01 11:02:40 +0530

    updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "https://shop-dot-v3.vercel.app/api/v2";
-export const backend_url = "https://shop-dot-v3.vercel.app/";
+export const server = "https://shop-dot-ut1x.vercel.app/api/v2";
+export const backend_url = "https://shop-dot-ut1x.vercel.app/";
