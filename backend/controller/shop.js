22f0c4f3f1662630f34fae5534946290440a1a76
savin8305 <kanhaji0402@gmail.com>
2023-12-01 14:14:07 +0530

    socket updated

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://shop-dot-v3-xyt1.vercel.app/seller/activation/${activationToken}`;
+    const activationUrl = `https://shop-dot-v3-m3z4.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
